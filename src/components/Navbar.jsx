cba9b3a69a068888dd4f4d92a431ee3c7f70fc40
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-04-08 16:39:29 +0500

    update images and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo" style={{ width: "50px" }} />
+        <img src={Logo} alt="Logo" style={{ width: "80px" }} />
       </div>
 
       <ul className="hidden md:flex">
