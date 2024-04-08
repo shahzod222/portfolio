038c7a4b170e96d4101950b03ed0dfa6fc2a40f5
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-04-08 16:18:40 +0500

    add all files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,133 @@
+import Logo from "../assets/logo.png";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaTelegram,
+} from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "50px" }} />
+      </div>
+
+      <ul className="hidden md:flex">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077b5]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/shahzod-ubaydullaev/"
+              target="_blank"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#171515]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href=""
+              target="_blank"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0088CC]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://t.me/shahzod_ub"
+              target="_blank"
+            >
+              Telegram <FaTelegram size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="../assets/resume.pdf"
+              download="resume.pdf"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import GraphiQl from "../assets/projects/graphiql.png";
+import RSLang from "../assets/projects/rslang.png";
+import Shelter from "../assets/projects/shelter.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "GraphiQL App",
+    image: GraphiQl,
+    github: "https://github.com/shahzod222/graphiql-app",
+    live: "graphiql-qibray.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "RS Lang App",
+    image: RSLang,
+    github: "https://github.com/shahzod222/rslang",
+    live: "app-rs-lang.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Shelter",
+    image: Shelter,
+    github:
+      "https://github.com/shahzod222/RSS-JSFE2022Q1/tree/gh-pages/shelter",
+    live: "https://shahzod222.github.io/RSS-JSFE2022Q1/shelter/main/",
+  },
+];
