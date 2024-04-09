ff096e9702e60dd0b5104127070c96f5132663de
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-04-09 15:19:53 +0500

    add new styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="z-10 fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo" style={{ width: "200px" }} />
       </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,6 @@
 import { data } from "../data/data.js";
+import { FaCode } from "react-icons/fa";
+import { BsBrowserChrome } from "react-icons/bs";
 
 const Work = () => {
   const project = data;
@@ -20,31 +22,36 @@ const Work = () => {
           {project.map((item, index) => (
             <div
               key={index}
-              style={{ backgroundImage: `url(${item.image})` }}
               className="shadow-lg shadow-[#040c16] group container rounded-md 
               flex justify-center text-center items-center mx-auto content-div "
             >
-              <div className="opacity-0 group-hover:opacity-100 duration-500">
-                <span className="text-2xl font bold  tracking-wider ">
-                  {item.name}
+              <img
+                className="w-full h-full group-hover:blur-sm duration-200 rounded-md"
+                src={item.image}
+              />
+              <div className="absolute z-2 opacity-0 group-hover:opacity-100 duration-500">
+                <span className="text-2xl font bold tracking-wider ">
+                  <div className="bg-white text-gray-700 px-4 py-2">
+                    {item.name}
+                  </div>
                 </span>
-                <div className="pt-8 text-center ">
+                <div className="pt-8 text-center flex justify-center items-center ">
                   {/* eslint-disable-next-line */}
                   <a href={item.github} target="_blank">
-                    <button
-                      className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg "
-                    >
+                    <button className="text-white group border-2 px-6 py-3 my-4 mx-1 flex items-center bg-pink-600 border-pink-600">
                       Code
+                      <span>
+                        <FaCode className="ml-3" />
+                      </span>
                     </button>
                   </a>
                   {/* eslint-disable-next-line */}
                   <a href={item.live} target="_blank">
-                    <button
-                      className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-                    >
-                      Live
+                    <button className="text-white group border-2 px-6 py-3 my-4 mx-1 flex items-center bg-pink-600 border-pink-600">
+                      Website
+                      <span>
+                        <BsBrowserChrome className="ml-3 " />
+                      </span>
                     </button>
                   </a>
                 </div>
