6c4ea4236b8ea44b009fa29c5e6425dbe7b46251
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-08-12 22:17:04 +0500

    remove react-tilt

--- src/utils/SkillElement.jsx
+++ src/utils/SkillElement.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { useAppContext } from "./AppContext";
-import { Tilt } from "react-tilt";
 
 const SkillElement = ({ name, img }) => {
   const { isDark } = useAppContext();
   return (
-    <Tilt>
-      <div
-        className={`shadow-md duration-500 rounded`}
-        style={{
-          boxShadow: `0 5px 7px -1px ${!isDark ? "#828282" : "#000102"}`,
-        }}
-      >
-        <img className="w-20 mx-auto" src={img} alt="HTML icon" />
-        <p className="my-4">{name}</p>
-      </div>
-    </Tilt>
+    <div
+      className={`shadow-md duration-500 rounded`}
+      style={{
+        boxShadow: `0 5px 7px -1px ${!isDark ? "#828282" : "#000102"}`,
+      }}
+    >
+      <img className="w-20 mx-auto" src={img} alt="HTML icon" />
+      <p className="my-4">{name}</p>
+    </div>
   );
 };
 
--- src/utils/WorkCard.jsx
+++ src/utils/WorkCard.jsx
@@ -3,48 +3,45 @@ import { FaCode } from "react-icons/fa";
 import { BsBrowserChrome } from "react-icons/bs";
 import CustomButton from "../utils/CustomButton";
 import { useAppContext } from "./AppContext";
-import { Tilt } from "react-tilt";
 
 const WorkCard = ({ item }) => {
   const { isDark, t } = useAppContext();
   return (
-    <Tilt>
-      <div
-        className={`group container rounded-md
+    <div
+      className={`group container rounded-md
               flex justify-center text-center items-center mx-auto content-div duration-500`}
-        style={{
-          boxShadow: `0 5px 7px -2px ${isDark ? "#828282" : "#000102"}`,
-        }}
-      >
-        <span
-          className="w-full h-full group-hover:blur-sm duration-200 rounded-md"
-          style={{ background: `url(${item.image})` }}
-        ></span>
-        <div className="absolute z-2 opacity-0 group-hover:opacity-100 duration-500">
-          <span className="text-2xl font bold tracking-wider ">
-            <div className="bg-white text-gray-700 px-4 py-2">{item.name}</div>
-          </span>
-          <div className="pt-8 text-center flex justify-center items-center ">
-            <a href={item.github} target="_blank" className="mx-2">
-              <CustomButton type="fill">
-                {t("work.button-code")}
-                <span>
-                  <FaCode className="ml-3" />
-                </span>
-              </CustomButton>
-            </a>
-            <a href={item.live} target="_blank" className="mx-2">
-              <CustomButton type="fill">
-                {t("work.button-website")}
-                <span>
-                  <BsBrowserChrome className="ml-3 " />
-                </span>
-              </CustomButton>
-            </a>
-          </div>
+      style={{
+        boxShadow: `0 5px 7px -2px ${isDark ? "#828282" : "#000102"}`,
+      }}
+    >
+      <span
+        className="w-full h-full group-hover:blur-sm duration-200 rounded-md"
+        style={{ background: `url(${item.image})` }}
+      ></span>
+      <div className="absolute z-2 opacity-0 group-hover:opacity-100 duration-500">
+        <span className="text-2xl font bold tracking-wider ">
+          <div className="bg-white text-gray-700 px-4 py-2">{item.name}</div>
+        </span>
+        <div className="pt-8 text-center flex justify-center items-center ">
+          <a href={item.github} target="_blank" className="mx-2">
+            <CustomButton type="fill">
+              {t("work.button-code")}
+              <span>
+                <FaCode className="ml-3" />
+              </span>
+            </CustomButton>
+          </a>
+          <a href={item.live} target="_blank" className="mx-2">
+            <CustomButton type="fill">
+              {t("work.button-website")}
+              <span>
+                <BsBrowserChrome className="ml-3 " />
+              </span>
+            </CustomButton>
+          </a>
         </div>
       </div>
-    </Tilt>
+    </div>
   );
 };
 
